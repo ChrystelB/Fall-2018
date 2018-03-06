9e72f57776d4604256edd907a2387fe30b723260
Chrystel <chrystelbardsley@gmail.com>
2018-03-06 16:01:15 -0700

    Started on state machines, but mostly worked on scriptable objects.

---
+++ Unity/CharacterDesign Assignments/Assets/Dance.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dance Moves", menuName = "Dance")]
+public class Dance : ScriptableObject {
+
+	public string danceMove;
+	public string description;
+
+	public int attack;
+	public int health;
+
+	public void Print() {
+		Debug.Log(danceMove + ": " + description + " The Dance attack = " + attack);
+	}
+}
---
+++ Unity/CharacterDesign Assignments/Assets/DanceActions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceActions : MonoBehaviour {
+
+	public Dance dance;
+
+	void Start () {
+		dance.Print ();
+	}
+
+
+}
